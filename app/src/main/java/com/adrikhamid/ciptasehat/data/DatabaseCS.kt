0c71e622d33f83b7eda3ff7890223c6b19219fd7
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-01-08 18:10:58 +0700

    membuat Database dengan Room

---
+++ app/src/main/java/com/adrikhamid/ciptasehat/data/DatabaseCS.kt
@@ -0,0 +1,30 @@
+package com.adrikhamid.ciptasehat.data
+
+import android.content.Context
+import android.provider.ContactsContract.Data
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.adrikhamid.ciptasehat.data.dao.AdminDao
+import com.adrikhamid.ciptasehat.data.entity.Admin
+import com.adrikhamid.ciptasehat.data.entity.Dokter
+import com.adrikhamid.ciptasehat.data.entity.Pasien
+import kotlinx.coroutines.InternalCoroutinesApi
+
+@Database(entities = [Dokter::class, Pasien::class], version = 1, exportSchema = false)
+abstract class DatabaseCS : RoomDatabase() {
+    abstract fun adminDao(): AdminDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseCS? = null
+
+        @OptIn(InternalCoroutinesApi::class)
+        fun getDatabase(context: Context): DatabaseCS {
+            return (Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, DatabaseCS::class.java, "ciptaSehat_database").build()
+                    .also { Instance = it }
+            })
+        }
+    }
+}
