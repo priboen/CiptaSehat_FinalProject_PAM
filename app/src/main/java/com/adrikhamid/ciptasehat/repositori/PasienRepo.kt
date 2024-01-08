60e00b5735c74d80ae86f0d89e8f8fcc0af0db51
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-01-08 17:37:43 +0700

    membuat repositori untuk entitas pasien

---
+++ app/src/main/java/com/adrikhamid/ciptasehat/repositori/PasienRepo.kt
@@ -0,0 +1,12 @@
+package com.adrikhamid.ciptasehat.repositori
+
+import com.adrikhamid.ciptasehat.data.entity.Pasien
+import kotlinx.coroutines.flow.Flow
+
+interface PasienRepo {
+    fun getAllPasienStream(): Flow<List<Pasien>>
+    fun getPasienStream(id: Int): Flow<Pasien?>
+    suspend fun insertPasien(pasien: Pasien)
+    suspend fun updatePasien(pasien: Pasien)
+    suspend fun deletePasien(pasien: Pasien)
+}
