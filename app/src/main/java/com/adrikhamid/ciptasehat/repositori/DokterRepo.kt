6ccb43db6a656d7b71d7358b56c83e989b6f670c
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-01-09 21:54:34 +0700

    menambahkan konstruktor Dao

--- app/src/main/java/com/adrikhamid/ciptasehat/repositori/DokterRepo.kt
+++ app/src/main/java/com/adrikhamid/ciptasehat/repositori/DokterRepo.kt
@@ -1,8 +1,10 @@
 package com.adrikhamid.ciptasehat.repositori
 
+import androidx.room.Dao
 import com.adrikhamid.ciptasehat.data.entity.Dokter
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface DokterRepo {
     fun getAllDokterStream():Flow<List<Dokter>>
     fun getDokterStream(id:Int):Flow<Dokter?>
--- app/src/main/java/com/adrikhamid/ciptasehat/repositori/PasienRepo.kt
+++ app/src/main/java/com/adrikhamid/ciptasehat/repositori/PasienRepo.kt
@@ -1,8 +1,10 @@
 package com.adrikhamid.ciptasehat.repositori
 
+import androidx.room.Dao
 import com.adrikhamid.ciptasehat.data.entity.Pasien
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface PasienRepo {
     fun getAllPasienStream(): Flow<List<Pasien>>
     fun getPasienStream(id: Int): Flow<Pasien?>
