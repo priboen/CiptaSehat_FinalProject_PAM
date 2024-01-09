11b45e8a5e4a79239fdbf20e3d62e4fafe00eeef
Dimas Damar Buana <dimasluxmundi@gmail.com>
2024-01-09 16:11:01 +0700

    change compile sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.adrikhamid.ciptasehat"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.adrikhamid.ciptasehat"
