59c6cc019963fd108190dae5f0bb7809da90cd14
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-01-09 13:54:50 +0700

    membuat objek data untuk pilihan spesialis

---
+++ app/src/main/java/com/adrikhamid/ciptasehat/data/objek/SpesialisDokter.kt
@@ -0,0 +1,14 @@
+package com.adrikhamid.ciptasehat.data.objek
+
+object SpesialisDokter {
+    val spesialis = listOf(
+        "Dokter Umum",
+        "Dokter Spesialis Gigi",
+        "Dokter Spesialis Saraf",
+        "Dokter Spesialis Anak",
+        "Dokter Spesialis Kulit dan Kelamin",
+        "Dokter Spesialis Mata",
+        "Dokter Spesialis THT",
+        "Dokter Spesialis Bedah",
+    )
+}
