54d06184857421780acf305fec50d357f779be7d
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-01-10 16:47:40 +0700

    menginisialisasi view model

--- app/src/main/java/com/adrikhamid/ciptasehat/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/adrikhamid/ciptasehat/ui/viewmodel/PenyediaViewModel.kt
@@ -6,6 +6,10 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.adrikhamid.ciptasehat.CiptaSehat
+import com.adrikhamid.ciptasehat.ui.viewmodel.berobat.BerobatDetailViewModel
+import com.adrikhamid.ciptasehat.ui.viewmodel.berobat.BerobatEditViewModel
+import com.adrikhamid.ciptasehat.ui.viewmodel.berobat.BerobatEntryViewModel
+import com.adrikhamid.ciptasehat.ui.viewmodel.berobat.BerobatHomeViewModel
 import com.adrikhamid.ciptasehat.ui.viewmodel.dokter.DokterDetailViewModel
 import com.adrikhamid.ciptasehat.ui.viewmodel.dokter.DokterEditViewModel
 import com.adrikhamid.ciptasehat.ui.viewmodel.dokter.DokterEntryViewModel
@@ -31,6 +35,7 @@ object PenyediaViewModel {
                 ciptaSehat().container.dokterRepo
             )
         }
+        //Pasien
         initializer { PasienHomeViewModel(ciptaSehat().container.pasienRepo) }
         initializer { PasienEntryViewModel(ciptaSehat().container.pasienRepo) }
         initializer {
@@ -44,6 +49,22 @@ object PenyediaViewModel {
                 createSavedStateHandle(),
                 ciptaSehat().container.pasienRepo
             )
+        }
+        //Berobat
+        initializer {
+            BerobatHomeViewModel(ciptaSehat().container.berobatRepo)
+        }
+        initializer {
+            BerobatEntryViewModel(ciptaSehat().container.berobatRepo)
+        }
+        initializer {
+            BerobatEditViewModel(
+                createSavedStateHandle(),
+                ciptaSehat().container.berobatRepo
+            )
+        }
+        initializer {
+            BerobatDetailViewModel(createSavedStateHandle(), ciptaSehat().container.berobatRepo)
         }
     }
 }
