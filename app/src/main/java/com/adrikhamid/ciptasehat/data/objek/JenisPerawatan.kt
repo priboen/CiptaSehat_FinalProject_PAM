df1237771aa6c43663bd3dd5ae2b2ce63ea3190f
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-01-10 15:58:12 +0700

    membuat object jenis perawatan

---
+++ app/src/main/java/com/adrikhamid/ciptasehat/data/objek/JenisPerawatan.kt
@@ -0,0 +1,10 @@
+package com.adrikhamid.ciptasehat.data.objek
+
+object JenisPerawatan {
+    val rawat = listOf(
+        "Inap Gawat Darurat",
+        "Unit Gawat Darurat",
+        "Konsultasi",
+        "Medical Check Up",
+    )
+}
