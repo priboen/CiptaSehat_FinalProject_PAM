42aa7ef80a65f6f96d6552b8b51e3b819f2059bd
Dimas Damar Buana <dimasluxmundi@gmail.com>
2024-01-12 02:31:28 +0700

    add Carousel

--- app/src/main/java/com/adrikhamid/ciptasehat/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/adrikhamid/ciptasehat/ui/screens/HomeScreen.kt
@@ -5,19 +5,29 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
@@ -29,6 +39,7 @@ import androidx.compose.ui.unit.sp
 import com.adrikhamid.ciptasehat.R
 import com.adrikhamid.ciptasehat.navigasi.CiptaSehatTopBar
 import com.adrikhamid.ciptasehat.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.delay
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
@@ -51,7 +62,7 @@ fun AppHomeScreen(
         }
     ) { innerPadding ->
         DashBoard(
-            onDokterClick = navigasiDokter ,
+            onDokterClick = navigasiDokter,
             onPasienClick = navigasiPasien,
             onBerobatClicl = navigasiBerobat,
             modifier = Modifier
@@ -72,6 +83,7 @@ fun DashBoard(
     val img_dokter = painterResource(id = R.drawable.dokter)
     val img_pasien = painterResource(id = R.drawable.pasien)
     val img_obat = painterResource(id = R.drawable.berobat)
+    val logo = painterResource(id = R.drawable.logo)
 
 
     Column(
@@ -82,8 +94,16 @@ fun DashBoard(
             .padding(
                 dimensionResource(id = R.dimen.padding_extra_large)
             )
-            .padding(top = dimensionResource(id = R.dimen.padding_extra_large)), horizontalAlignment = Alignment.CenterHorizontally
+            .padding(top = dimensionResource(id = R.dimen.padding_extra_large)),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(painter = logo, contentDescription = null, Modifier.requiredSize(150.dp))
+        Text(
+            text = "Cipta Sehat",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Divider()
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -247,9 +267,78 @@ fun DashBoard(
                     )
                 }
 
-
             }
 
+        }
+        Carousel()
+    }
+}
+
+@Composable
+fun Carousel() {
+    val images = listOf(
+        R.drawable.banner,
+        R.drawable.banner1,
+        R.drawable.banner3,
+    )
+    ImagesSliderWithIndicator(images = images)
+}
+
+@Composable
+fun ImageSliderItem(
+    imageRes: Int
+) {
+    Image(
+        painter = painterResource(id = imageRes),
+        contentDescription = null,
+        contentScale = ContentScale.Fit,
+        modifier = Modifier
+            .height(150.dp)
+            .fillMaxWidth()
+    )
+}
+
+@Composable
+fun Indicator(active: Boolean) {
+    val color = if (active) Color.Cyan else Color.Gray
+    val size = if (active) 12.dp else 7.dp
+    Box(
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(color)
+            .size(size)
+    )
+}
+
+@Composable
+fun ImagesSliderWithIndicator(images: List<Int>) {
+    val currentIndex = remember {
+        mutableStateOf(0)
+    }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(3000)
+            currentIndex.value = (currentIndex.value + 1) % images.size
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(top = 10.dp)
+    ) {
+        ImageSliderItem(imageRes = images[currentIndex.value])
+    }
+    Row(
+        modifier = Modifier.padding(top = 10.dp, bottom = 55.dp)
+    ) {
+        images.forEachIndexed { index, i ->
+            Indicator(
+                active = index == currentIndex.value
+            )
+            if (index < images.size - 1) {
+                Spacer(modifier = Modifier.width(15.dp))
+            }
         }
     }
 }
